f9d222392c5f57e14d2a735da7a99e274f565e9f
ritik1305 <60670834+ritik1305@users.noreply.github.com>
2020-07-25 21:15:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-/*
-Solution of assignment 4:
-
-Expected output:
-
-Hello Ritik
-Good Bye Akash
-Good Bye Kuldeep
-Good Bye Heramb
-Hello Pranmya
-Hello Choudhary
-Hello Aman
-Hello Bhargav
-Hello Aijaz
-Good Bye Arman
-*/
-
 (function() {
     var names = ["Ritik", "Akash", "Kuldeep", "Heramb", "Pranmya", "Choudhary", "Aman", "Bhargav", "Aijaz", "Arman"];
     for (var i = 0; i < names.length; i++) {
