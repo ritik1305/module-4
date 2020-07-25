b4fab36ebbcc4bf8e7100e6a4331a92770431469
ritik1305 <60670834+ritik1305@users.noreply.github.com>
2020-07-25 21:23:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
+/*
+Solution of assignment 4:
+
+Expected output:
+
+Hello Yaakov
+Good Bye John
+Good Bye Jen
+Good Bye Jason
+Hello Paul
+Hello Frank
+Hello Larry
+Hello Paula
+Hello Laura
+Good Bye Jim
+*/
+
 (function() {
-    var names = ["Ritik", "Akash", "Kuldeep", "Heramb", "Pranmya", "Choudhary", "Aman", "Bhargav", "Aijaz", "Arman"];
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
-        if (firstLetter === 'a') {
+        if (firstLetter === 'j') {
             byeSpeaker.speak(names[i]);
         } else {
             helloSpeaker.speak(names[i]);
