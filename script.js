167e544fcf700692c777384d195fd2d6b24ca123
ritik1305 <60670834+ritik1305@users.noreply.github.com>
2020-07-25 21:15:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ Good Bye Arman
     var names = ["Ritik", "Akash", "Kuldeep", "Heramb", "Pranmya", "Choudhary", "Aman", "Bhargav", "Aijaz", "Arman"];
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
-        if (firstLetter === 'j') {
+        if (firstLetter === 'a') {
             byeSpeaker.speak(names[i]);
         } else {
             helloSpeaker.speak(names[i]);
